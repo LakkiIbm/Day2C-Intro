eb26641ec0cf450d66a7419aeaadfaf1f05bf7e0
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-01 16:59:18 +0530

    Task 6: Show the working of pass by value, pass by reference and pass by out using any example. Task 7: Show the working of 1D and 2D array in C# with any example

---
+++ FirstConsoleApp/Array1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+//------------------------------1D Array and its features-----------------------------
+//namespace FirstConsoleApp
+//{
+//    class Array1
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[] fruits = { "Lichi", "Guava", "Apple", "Grapes", "Mango" };
+//            Console.WriteLine("Number of fruits I like: " + fruits.Length);
+//            Console.WriteLine("Names of the fruits:");
+//            foreach (string fruit in fruits)
+//            {
+//                Console.WriteLine(fruit);
+//            }
+//            Array.Sort(fruits);
+//            Console.WriteLine("Names of the fruits in alphabetical order:");
+//            foreach (string fruit in fruits)
+//            {
+//                Console.WriteLine(fruit);
+//            }
+
+//            int[] nos = { 4, 6, 1, 3, 10, 2 };
+//            Console.WriteLine("No. of integers in the array: " + nos.Length);
+//            foreach (int i in nos)
+//                Console.WriteLine(i);
+//            Console.WriteLine("Maximum integer in the array: " + nos.Max());
+//            Console.WriteLine("Minimum integer in the array: " + nos.Min());
+//            Console.WriteLine("Sum of integers in the array: " + nos.Sum());
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//------------------------------2D Array and its features-----------------------------
+namespace FirstConsoleApp
+{
+    class Array1
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[3,2]{ {4, 6 }, { 1, 3 }, { 10, 2 } };
+
+            Console.WriteLine("Total no. of integers in the array: " + arr.Length);
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 2; j++)
+                    Console.WriteLine("a[{0},{1}] = {2}",i,j,arr[i,j]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ FirstConsoleApp/passing.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+//----------------------------Passing by Value-------------------------------
+
+//namespace FirstConsoleApp
+//{
+//    class passing
+//    {
+//        void doSwap(int x, int y)
+//        {
+//            int temp;
+//            temp = x;
+//            x = y;
+//            y = temp;
+//            Console.WriteLine("After swapping in method: x = {0}, y = {1}",x,y);
+//        }
+//        static void Main(string[] args)
+//        {
+//            passing n = new passing();
+//            int a = 1;
+//            int b = 2;
+
+//            Console.WriteLine("Before swapping: a = {0}, b = {1}", a, b);
+//            n.doSwap(a,b);
+//            Console.WriteLine("After swapping: a = {0}, b = {1}", a, b);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//----------------------------Passing by Reference-------------------------------
+//namespace FirstConsoleApp
+//{
+//    class passing
+//    {
+//        void doSwap(ref int x, ref int y)
+//        {
+//            int temp;
+//            temp = x;
+//            x = y;
+//            y = temp;
+//        }
+//        static void Main(string[] args)
+//        {
+//            passing n = new passing();
+//            int a = 1;
+//            int b = 2;
+
+//            Console.WriteLine("Before swapping: a = {0}, b = {1}", a, b);
+//            n.doSwap(ref a, ref b);
+//            Console.WriteLine("After swapping: a = {0}, b = {1}", a, b);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//----------------------------Passing by Out-------------------------------
+//namespace FirstConsoleApp
+//{
+//    class passing
+//    {
+//        void getValues(out int x, out int y)
+//        {
+//            x = 10;
+//            y = 20;
+//        }
+//        static void Main(string[] args)
+//        {
+//            passing n = new passing();
+//            int a = 1;
+//            int b = 2;
+
+//            Console.WriteLine("Before passing in method: a = {0}, b = {1}", a, b);
+//            n.getValues(out a, out b);
+//            Console.WriteLine("After passing in method: a = {0}, b = {1}", a, b);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
