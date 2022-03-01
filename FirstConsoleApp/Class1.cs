5aa78ce87267b8a15ce6087acba80a6d688cfb1c
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-01 12:54:10 +0530

    Add project files.

---
+++ FirstConsoleApp/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Enter the number: ");
+            n=int.Parse(Console.ReadLine());
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program to find greatest among 3 nos
+            //int a, b, c;
+            //Console.WriteLine("Enter 3 values:");
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+            //c = int.Parse(Console.ReadLine());
+            //if (a > b)
+            //{
+            //    if (a > c)
+            //    {
+            //        Console.WriteLine("A = {0} is the greatest", a);
+            //    }
+            //}
+            //else if (b > c)
+            //{
+            //    Console.WriteLine("B = {0} is the greatest", b);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("C = {0} is the greatest", c);
+            //}
+
+
+            //Program to swap 2 nos without 3rd variable
+            //int a, b;
+            //Console.WriteLine("Enter 2 values:");
+            //a = int.Parse(Console.ReadLine());
+            //b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Before Swapping: a = {0}, b = {1}", a, b);
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            //Console.WriteLine("After Swapping: a = {0}, b = {1}", a, b);
+
+
+            //Program to get emp details and display them
+            string name, add, city;
+            int empID, age;
+            Console.Write("Name: ");
+            name = Console.ReadLine();
+            Console.Write("Employee ID: ");
+            empID = int.Parse(Console.ReadLine());
+            Console.Write("Address: ");
+            add = Console.ReadLine();
+            Console.Write("City: ");
+            city = Console.ReadLine();
+            Console.Write("Age: ");
+            age = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Below are the details of {0}:", name);
+            Console.WriteLine("Employee ID : " + empID);
+            Console.WriteLine("Age : " + age);
+            Console.WriteLine("Address : " + add);
+            Console.WriteLine("City : " + city);
+            Console.ReadLine();
+        }
+    }
+}
